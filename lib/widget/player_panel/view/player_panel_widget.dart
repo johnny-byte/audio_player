a7d28529149c21412fe0b61bfc43129367ee8f16
None <>
2022-07-25 11:08:18 +0800

    fix ui

--- lib/widget/player_panel/view/player_panel_widget.dart
+++ lib/widget/player_panel/view/player_panel_widget.dart
@@ -10,42 +10,52 @@ class PlayerPanel extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => PlayerPanelCubit(context.read<Player>()),
-      child: SizedBox(
-        height: 64,
-        child: BlocBuilder<PlayerPanelCubit, PlayerPanelState>(
-          builder: (context, state) {
-            if (state is PlayerPanelDisable) {
-              return const SizedBox.shrink();
-            }
-            return Column(children: [
+      child: BlocBuilder<PlayerPanelCubit, PlayerPanelState>(
+        builder: (context, state) {
+          if (state is PlayerPanelDisable) {
+            return const SizedBox.shrink();
+          }
+          return SizedBox(
+            height: 64,
+            child: Column(children: [
               LinearProgressIndicator(
                 value: state.position.inMilliseconds /
                     state.duration.inMilliseconds,
               ),
-              Row(children: [
-                IconButton(
-                  onPressed: context.read<PlayerPanelCubit>().seekToPrevious,
-                  icon: const Icon(Icons.skip_previous),
-                ),
-                if (state is PlayerPanelPause)
-                  IconButton(
-                      onPressed: context.read<PlayerPanelCubit>().play,
-                      icon: const Icon(Icons.play_arrow))
-                else if (state is PlayerPanelPlaying)
-                  IconButton(
-                      onPressed: context.read<PlayerPanelCubit>().pause,
-                      icon: const Icon(Icons.pause))
-                else
-                  throw Exception("unexpected state: $state"),
-                IconButton(
-                  onPressed: context.read<PlayerPanelCubit>().seekToNext,
-                  icon: const Icon(Icons.skip_next),
-                ),
-              ]),
-            ]);
-          },
-        ),
+              Expanded(child: ControlButtonsLine(state: state)),
+            ]),
+          );
+        },
       ),
     );
+  }
+}
+
+class ControlButtonsLine extends StatelessWidget {
+  final PlayerPanelState state;
+  const ControlButtonsLine({Key? key, required this.state}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      IconButton(
+        onPressed: context.read<PlayerPanelCubit>().seekToPrevious,
+        icon: const Icon(Icons.skip_previous),
+      ),
+      if (state is PlayerPanelPause)
+        IconButton(
+            onPressed: context.read<PlayerPanelCubit>().play,
+            icon: const Icon(Icons.play_arrow))
+      else if (state is PlayerPanelPlaying)
+        IconButton(
+            onPressed: context.read<PlayerPanelCubit>().pause,
+            icon: const Icon(Icons.pause))
+      else
+        throw Exception("unexpected state: $state"),
+      IconButton(
+        onPressed: context.read<PlayerPanelCubit>().seekToNext,
+        icon: const Icon(Icons.skip_next),
+      ),
+    ]);
   }
 }
